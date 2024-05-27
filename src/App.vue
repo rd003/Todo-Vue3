2f860a574b4d68c1ab8ba562edb1724fefbea9b5
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-05-27 20:12:19 +0530

    feat: todo setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<style>
+.ctr {
+  padding: 20px;
+  font-size: 16px;
+}
+
+button {
+  padding: 5px 10px;
+  cursor: pointer;
+}
+</style>
+
+<script setup lang="ts">
+import { computed, ref, reactive, watch } from 'vue'
+import Todo from './todo/todo.vue'
+</script>
+
+<template>
+  <div class="ctr">
+    <router-view></router-view>
+  </div>
+</template>
